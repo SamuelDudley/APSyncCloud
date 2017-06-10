b2d07aea9e46b21750b22ca9050bb5a33b53e436
samuel dudley <dudley.samuel@gmail.com>
2017-06-11 02:06:09 +0930

    mail - move to single quotes

--- mail.py
+++ mail.py
@@ -13,7 +13,7 @@ def send_email(user, pwd, recipient, subject, body):
     """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
     try:
         # SMTP_SSL Example
-        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server_ssl.ehlo() # optional, called by login()
         server_ssl.login(gmail_user, gmail_pwd)  
         # ssl server doesn't support or need tls, so don't call server_ssl.starttls() 
@@ -22,5 +22,5 @@ def send_email(user, pwd, recipient, subject, body):
         server_ssl.close()
         print('successfully sent the mail')
     except Exception as e:
-        print("failed to send mail, {0}".format(e))
+        print('failed to send mail, {0}'.format(e))
         
